0e949fc163b8500a9bcf74ffb72519eb0d62cb4a
Martin Chaine <martin@casimir.me>
2024-11-25 20:36:57 +0100

    feat(control): implement user update

--- server/control/users.go
+++ server/control/users.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/casimir/freon/auth"
 	"github.com/casimir/freon/common"
+	"github.com/casimir/freon/database"
 	"github.com/casimir/freon/serialize"
 	"github.com/gin-gonic/gin"
 )
@@ -64,7 +65,66 @@ func registerUsersRoutes(r *gin.RouterGroup) {
 		common.DescribeAndRespond(c, user)
 	})
 	r.PUT("/:id", func(c *gin.Context) {
-		c.Status(http.StatusNotImplemented)
+		id := c.Param("id")
+		currentUser := auth.GetUser(c)
+		if !(currentUser.ID.String() == id || currentUser.IsSuperuser) {
+			c.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+
+		user, err := auth.FindUserByID(id)
+		if err != nil {
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+		if user == nil {
+			c.AbortWithStatus(http.StatusNotFound)
+			return
+		}
+
+		var payload struct {
+			Username     string `json:"username"`
+			OldPassword  string `json:"old_password"`
+			NewPassword1 string `json:"new_password1"`
+			NewPassword2 string `json:"new_password2"`
+		}
+		if err := c.ShouldBindJSON(&payload); err != nil {
+			c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+				"message": err.Error(),
+			})
+		}
+
+		dirty := false
+		if payload.Username != "" {
+			user.Username = payload.Username
+			dirty = true
+		}
+		if payload.OldPassword != "" || payload.NewPassword1 != "" || payload.NewPassword2 != "" {
+			if payload.NewPassword1 != payload.NewPassword2 {
+				c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
+					"message": "new passwords do not match",
+				})
+				return
+			}
+			if !user.CheckPassword(payload.OldPassword) {
+				c.AbortWithStatusJSON(http.StatusForbidden, gin.H{
+					"message": "incorrect password",
+				})
+				return
+			}
+			user.SetPassword(payload.NewPassword1)
+			dirty = true
+		}
+
+		if dirty {
+			if err := database.DB.Save(user).Error; err != nil {
+				c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+					"message": err.Error(),
+				})
+				return
+			}
+			c.Status(http.StatusOK)
+		}
 	})
 	r.DELETE("/:id", auth.IsSuperUser(), func(c *gin.Context) {
 		id := c.Param("id")
--- server/control/users_test.go
+++ server/control/users_test.go
@@ -1,11 +1,11 @@
 package control
 
 import (
-	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/casimir/freon/auth"
@@ -14,12 +14,15 @@ import (
 	"github.com/google/uuid"
 )
 
+var sessionUserIncr = 0
+
 func setupRouter(t *testing.T, adminSession ...bool) (*gin.Engine, *auth.User) {
 	var isAdmin bool
 	if len(adminSession) == 1 {
 		isAdmin = adminSession[0]
 	}
-	user := auth.MustCreateUser(t.Name()+"_session", "", isAdmin)
+	user := auth.MustCreateUser(fmt.Sprintf("%s_session_%d", t.Name(), sessionUserIncr), "", isAdmin)
+	sessionUserIncr += 1
 
 	gin.SetMode(gin.TestMode)
 	router := gin.New()
@@ -29,6 +32,8 @@ func setupRouter(t *testing.T, adminSession ...bool) (*gin.Engine, *auth.User) {
 }
 
 func assertUserQuery(t *testing.T, router *gin.Engine, expectedID, expectedUsername string) {
+	t.Helper()
+
 	w := httptest.NewRecorder()
 	req, _ := http.NewRequest("GET", "/users/"+expectedID, nil)
 	router.ServeHTTP(w, req)
@@ -59,6 +64,20 @@ func assertUserQuery(t *testing.T, router *gin.Engine, expectedID, expectedUsern
 	}
 }
 
+func assertHttpStatus(t *testing.T, req *http.Request, expected int) *httptest.ResponseRecorder {
+	t.Helper()
+
+	router, _ := setupRouter(t)
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, req)
+
+	if expected != w.Code {
+		t.Fatalf("expected status %d, got %d", expected, w.Code)
+	}
+
+	return w
+}
+
 func TestUserRetrieve(t *testing.T) {
 	otherUser := auth.MustCreateUser(t.Name(), "", false)
 
@@ -68,8 +87,8 @@ func TestUserRetrieve(t *testing.T) {
 
 func TestUserCreate(t *testing.T) {
 	username := t.Name()
-	payload := []byte(fmt.Sprintf(`{"username":%q,"password":"test"}`, username))
-	req, _ := http.NewRequest("POST", "/users", bytes.NewReader(payload))
+	payload := fmt.Sprintf(`{"username":%q,"password":"test"}`, username)
+	req, _ := http.NewRequest("POST", "/users", strings.NewReader(payload))
 
 	t.Run("NotAdmin", func(t *testing.T) {
 		router, _ := setupRouter(t)
@@ -102,18 +121,53 @@ func TestUserCreate(t *testing.T) {
 	})
 }
 
+func TestUserUpdate(t *testing.T) {
+	username1 := t.Name() + "_1"
+	user := auth.MustCreateUser(username1, "", false)
+
+	username2 := t.Name() + "_2"
+	payload1 := fmt.Sprintf(`{"username":%q}`, username2)
+	req1, _ := http.NewRequest("PUT", "/users/"+user.ID.String(), strings.NewReader(payload1))
+
+	t.Run("NeitherAdminNorSelf", func(t *testing.T) {
+		assertHttpStatus(t, req1, http.StatusForbidden)
+	})
+
+	t.Run("Admin", func(t *testing.T) {
+		router, _ := setupRouter(t, true)
+		w := httptest.NewRecorder()
+		router.ServeHTTP(w, req1)
+
+		if http.StatusOK != w.Code {
+			t.Fatalf("expected status %d, got %d", http.StatusOK, w.Code)
+		}
+
+		assertUserQuery(t, router, user.ID.String(), username2)
+	})
+
+	t.Run("Self", func(t *testing.T) {
+		router, self := setupRouter(t)
+		username3 := t.Name() + "_3"
+		payload2 := fmt.Sprintf(`{"username":%q}`, username3)
+		req2, _ := http.NewRequest("PUT", "/users/"+self.ID.String(), strings.NewReader(payload2))
+
+		w := httptest.NewRecorder()
+		router.ServeHTTP(w, req2)
+
+		if http.StatusOK != w.Code {
+			t.Fatalf("expected status %d, got %d", http.StatusOK, w.Code)
+		}
+
+		assertUserQuery(t, router, self.ID.String(), username3)
+	})
+}
+
 func TestUserDelete(t *testing.T) {
 	user := auth.MustCreateUser(t.Name(), "", false)
 
 	t.Run("NotAdmin", func(t *testing.T) {
-		router, _ := setupRouter(t)
-		w := httptest.NewRecorder()
 		req, _ := http.NewRequest("DELETE", "/users/"+user.ID.String(), nil)
-		router.ServeHTTP(w, req)
-
-		if http.StatusForbidden != w.Code {
-			t.Fatalf("expected status %d, got %d", http.StatusForbidden, w.Code)
-		}
+		assertHttpStatus(t, req, http.StatusForbidden)
 	})
 
 	t.Run("Admin", func(t *testing.T) {
