71e107feb5f42c37b0d47f208b8ac8ad25966360
Martin Chaine <martin@casimir.me>
2025-05-22 17:12:58 +0200

    fix(settings): don't allow local hosts by default and handle lists

--- freon_server/freon_server/settings.py
+++ freon_server/freon_server/settings.py
@@ -22,9 +22,9 @@ VERSION = env("VERSION", default="unknown")
 
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
-if host := env("FREON_DOMAIN", default=""):
-    ALLOWED_HOSTS.append(host)
+ALLOWED_HOSTS = []
+if hosts := env.list("FREON_HOSTS", cast=str, default=None):
+    ALLOWED_HOSTS += hosts
 INTERNAL_IPS = ["127.0.0.1"]
 
 
